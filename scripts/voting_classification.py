a51efa531ce2a7e9c6aa8d78dc8d273c5900a0ae
Jason Chang <jasonchang0@berkeley.edu>
2018-11-14 11:03:51 -0800

    f1_score metrics

--- scripts/voting_classification.py
+++ scripts/voting_classification.py
@@ -1,11 +1,13 @@
 import os
+import sys
 import numpy as np
 import pandas as pd
 from statistics import mean
 import matplotlib.pyplot as plt
 from matplotlib import style
 from sklearn.decomposition import PCA
-from sklearn import utils, preprocessing
+from sklearn import utils, preprocessing, neighbors, metrics
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.feature_selection import RFECV
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
@@ -15,9 +17,15 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, VotingClassifier
 import pickle
 
+
 style.use('fivethirtyeight')
 
 os.chdir('../data')
+
+
+orig_stdout = sys.stdout
+file = open('cross_validation.txt', 'w')
+sys.stdout = file
 
 open_file = open('reduced_features.pickle', 'rb')
 feature_rank = pickle.load(open_file)
@@ -27,34 +35,53 @@ filename = 'fs_master_dataset.csv.gz'
 df = pd.read_csv(filepath_or_buffer=filename, compression='gzip')
 # print(df.head())
 
+
+KNeighbors_classifier = neighbors.KNeighborsClassifier(weights='distance', algorithm='kd_tree', n_jobs=-1)
+QDA_classifier = QuadraticDiscriminantAnalysis()
+
 '''
 Naive Bayes classifier
 posterior = prior occurrences * likelihood / evidence
 
 *Requires non-negative X inputs
 '''
-MNB_classifier = MultinomialNB()
-GaussianNB_classifier = GaussianNB()
-BernoulliNB_classifier = BernoulliNB()
+# MNB_classifier = MultinomialNB()
+# GaussianNB_classifier = GaussianNB()
+# BernoulliNB_classifier = BernoulliNB()
 
 DecisionTree_classifier = DecisionTreeClassifier()
-LogisticRegression_classifier = LogisticRegression()
+LogisticRegression_classifier = LogisticRegression(solver='lbfgs', max_iter=1000, n_jobs=-1)
 SGD_classifier = SGDClassifier(penalty='elasticnet', learning_rate='optimal', n_jobs=-1)
 RandomForest_classifier = RandomForestClassifier(n_estimators=50, n_jobs=-1)
 AdaBoost_classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.5)
 
-
 '''
 Support Vector Machines
 '''
-SVC_classifier = SVC()
-LinearSVC_classifier = LinearSVC()
-NuSVC_classifier = NuSVC(nu=0.9, max_iter=1000)
+SVC_classifier = SVC(kernel='rbf', max_iter=1000)
+LinearSVC_classifier = LinearSVC(penalty='l2', loss='squared_hinge', max_iter=1000)
+# NuSVC_classifier = NuSVC(nu=0.2, max_iter=1000)
+
+# clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
+clf = VotingClassifier(estimators=[('KNC', KNeighbors_classifier), ('QDA', QDA_classifier),
+                                   ('Log', LogisticRegression_classifier),
+                                   ('SGD', SGD_classifier), ('RF', RandomForest_classifier),
+                                   ('SVC', SVC_classifier), ('LSVC', LinearSVC_classifier),
+                                   ('DTC', DecisionTree_classifier),
+                                   ('ABC', AdaBoost_classifier)], voting='hard', n_jobs=-1)
+
+# Serialize classification model for future implementation
+save_file = open('voting_classifier.pickle', 'wb')
+pickle.dump(clf, save_file)
+save_file.close()
+
 
 accuracies = []
+f1_scores = []
 
 for _ in range(10):
-    part_df = utils.shuffle(df, random_state=None, n_samples=100000)
+
+    part_df = utils.shuffle(df, random_state=None, n_samples=250000)
 
     X = part_df.drop(['label'], axis=1, inplace=False)
 
@@ -66,15 +93,9 @@ for _ in range(10):
     y = y.astype(int)
 
     X = np.array(X).reshape(len(part_df.index), 341)
-    y = np.array(y).reshape(len(part_df.index), -1)
+    y = np.array(y).reshape(len(part_df.index), )
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-    clf = VotingClassifier(estimators=[('Log', LogisticRegression_classifier),
-                                       ('SGD', SGD_classifier), ('RF', RandomForest_classifier),
-                                       ('SVC', SVC_classifier), ('LSVC', LinearSVC_classifier),
-                                       ('NuSVC', NuSVC_classifier), ('DTC', DecisionTree_classifier),
-                                       ('ABC', AdaBoost_classifier)], voting='hard', n_jobs=-1)
 
     clf = clf.fit(X_train, y_train)
 
@@ -82,7 +103,23 @@ for _ in range(10):
     print('Accuracy:', accuracy)
     accuracies.append(accuracy)
 
-print(mean(accuracies))
+    f1_score = metrics.f1_score(y_true=y_test, y_pred=clf.predict(X=X_test), average='weighted')
+    print('F1-Score:', f1_score)
+    f1_scores.append(f1_score)
+
+
+print('Mean Accuracy: {}'.format(mean(accuracies)))
+print('Mean F1-Score: {}'.format(mean(f1_scores)))
+
+
+sys.stdout = orig_stdout
+file.close()
+
+
+
+
+
+
 
 
 
--- scripts/wrapper_feature_selection.py
+++ scripts/wrapper_feature_selection.py
@@ -1,10 +1,12 @@
+import sys
 import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
 from sklearn.decomposition import PCA
-from sklearn import utils, preprocessing
+from sklearn import utils, preprocessing, neighbors
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_selection import RFECV
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
@@ -18,6 +20,10 @@ style.use('fivethirtyeight')
 
 os.chdir('../data')
 
+orig_stdout = sys.stdout
+file = open('feature_rank.txt', 'w')
+sys.stdout = file
+
 filename = 'fs_master_dataset.csv.gz'
 df = pd.read_csv(filepath_or_buffer=filename, compression='gzip')
 # print(df.head())
@@ -30,16 +36,19 @@ X = X.astype(float)
 y = df[['label']]
 y = y.astype(int)
 
+KNeighbors_classifier = neighbors.KNeighborsClassifier(weights='distance', algorithm='kd_tree', n_jobs=-1)
+QDA_classifier = QuadraticDiscriminantAnalysis()
+
 '''
 Naive Bayes classifier
 posterior = prior occurrences * likelihood / evidence
 '''
-MNB_classifier = MultinomialNB()
-GaussianNB_classifier = GaussianNB()
-BernoulliNB_classifier = BernoulliNB()
+# MNB_classifier = MultinomialNB()
+# GaussianNB_classifier = GaussianNB()
+# BernoulliNB_classifier = BernoulliNB()
 
 DecisionTree_classifier = DecisionTreeClassifier()
-LogisticRegression_classifier = LogisticRegression()
+LogisticRegression_classifier = LogisticRegression(solver='lbfgs', max_iter=1000, n_jobs=-1)
 SGD_classifier = SGDClassifier(penalty='elasticnet', learning_rate='optimal', n_jobs=-1)
 RandomForest_classifier = RandomForestClassifier(n_estimators=50, n_jobs=-1)
 AdaBoost_classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.5)
@@ -47,17 +56,17 @@ AdaBoost_classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.5)
 '''
 Support Vector Machines
 '''
-SVC_classifier = SVC()
-LinearSVC_classifier = LinearSVC()
-NuSVC_classifier = NuSVC()
+SVC_classifier = SVC(kernel='rbf', max_iter=1000)
+LinearSVC_classifier = LinearSVC(penalty='l2', loss='squared_hinge', max_iter=1000)
+# NuSVC_classifier = NuSVC()
 
-combined_classifier = VotingClassifier(estimators=[('MNB', MNB_classifier), ('GNB', GaussianNB_classifier),
-                                                   ('BNB', BernoulliNB_classifier),
-                                                   ('Log', LogisticRegression_classifier),
-                                                   ('SGD', SGD_classifier), ('RF', RandomForest_classifier),
-                                                   ('SVC', SVC_classifier), ('LSVC', LinearSVC_classifier),
-                                                   ('NuSVC', NuSVC_classifier), ('DTC', DecisionTree_classifier),
-                                                   ('ABC', AdaBoost_classifier)], voting='hard')
+combined_classifier = VotingClassifier(
+    estimators=[('KNC', KNeighbors_classifier), ('QDA', QDA_classifier),
+                ('Log', LogisticRegression_classifier),
+                ('SGD', SGD_classifier), ('RF', RandomForest_classifier),
+                ('SVC', SVC_classifier), ('LSVC', LinearSVC_classifier),
+                ('DTC', DecisionTree_classifier),
+                ('ABC', AdaBoost_classifier)], voting='hard', n_jobs=-1)
 
 rfecv = RFECV(estimator=RandomForest_classifier, step=1, cv=StratifiedKFold(n_splits=3),
               scoring='accuracy', n_jobs=-1)
@@ -82,6 +91,9 @@ print(np.array(feature_rank)[:, 0])
 save_file = open('reduced_features.pickle', 'wb')
 pickle.dump(np.array(feature_rank)[:, 0], save_file)
 save_file.close()
+
+sys.stdout = orig_stdout
+file.close()
 
 # Plot number of features VS. cross-validation scores
 plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_, linestyle="-", color='b', linewidth=5)
